0a51927227dc912552b987375df8966f182d7bf3
jesus95santana <jesus95santana@yahoo.com>
2023-07-18 16:44:49 -0400

    header complete | missing responsive design

--- lanyardDevelopment/resources/views/components/header.blade.php
+++ lanyardDevelopment/resources/views/components/header.blade.php
@@ -1,3 +1,18 @@
-<header>
-    <p>Header</p>
+<header class="border-b-[2px] border-black">
+    <div class="headerContainer flex justify-between align-middle m-auto max-w-screen-lg flex-wrap">
+        <figure class="Logo">
+            <img src="./img/LC-LOGO.gif" alt="Logo">
+        </figure>
+        <nav class="border flex">
+            <ul class="flex border items-center">
+                <li class="p-4"><a href="">EXCAVATION SITES</a></li>
+                <li class="p-4"><a href="">CONTACT</a></li>
+            </ul>
+        </nav>
+        <div class="headerContactContainer flex border items-center">
+            <figure><img src="./img/LD-Phone-ICON.png" alt="Facebook Icon"></figure>
+            <figure><img src="./img/LD-Phone-ICON.png" alt="Phone Icon"></figure>
+            <p>912 330-8351</p>
+        </div>
+    </div>
 </header>
