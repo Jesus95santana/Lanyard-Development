48b7419cfe76bf4ce4e864d22a1e880a3d1a2d99
jesus95santana <jesus95santana@yahoo.com>
2023-07-19 10:53:00 -0400

    added Footer Icon

--- lanyardDevelopment/resources/views/components/footer.blade.php
+++ lanyardDevelopment/resources/views/components/footer.blade.php
@@ -1,3 +1,8 @@
 <footer>
-    <p>Footer</p>
+    <div class="footerContainer">
+        <figure class="footerIcon"><img src="LC-HomeFooterBar.png" alt="Footer Icon"></figure>
+        <div class="footerInfo">
+            <h2>Lanyard Development Inc.</h2>
+        </div>
+    </div>
 </footer>
--- lanyardDevelopment/resources/views/components/header.blade.php
+++ lanyardDevelopment/resources/views/components/header.blade.php
@@ -3,13 +3,13 @@
         <figure class="Logo">
             <img src="./img/LC-LOGO.gif" alt="Logo">
         </figure>
-        <nav class="border flex">
-            <ul class="flex border items-center">
-                <li class="p-4 font-bold text-gray-500 font-[sans-serif]"><a href="">EXCAVATION SITES</a></li>
-                <li class="p-4 font-bold text-gray-500 font-[sans-serif]"><a href="">CONTACT</a></li>
+        <nav class="flex">
+            <ul class="flex items-center">
+                <li class="px-4 py-2 font-bold text-gray-500 font-[sans-serif] hover:bg-[#234162] hover:text-white hover:cursor-pointer"><a href="">EXCAVATION SITES</a></li>
+                <li class="px-4 py-2 font-bold text-gray-500 font-[sans-serif] hover:bg-[#234162] hover:text-white hover:cursor-pointer"><a href="">CONTACT</a></li>
             </ul>
         </nav>
-        <div class="headerContactContainer flex border items-center p-2">
+        <div class="headerContactContainer flex items-center p-2">
             <figure class="p-1"><img src="./img/LD-Phone-ICON.png" alt="Facebook Icon"></figure>
             <figure class="p-1"><img src="./img/LD-Phone-ICON.png" alt="Phone Icon"></figure>
             <p class="p-1 text-lg text-gray-500 font-medium">912 330-8351</p>
