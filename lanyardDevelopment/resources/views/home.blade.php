cde5c165e31f1f608376c1e0deec249fb8792745
jesus95santana <jesus95santana@yahoo.com>
2023-07-19 15:50:26 -0400

    homepage added textbox && location boxes

--- lanyardDevelopment/resources/views/home.blade.php
+++ lanyardDevelopment/resources/views/home.blade.php
@@ -3,13 +3,29 @@
         <div class="imageCarousel">
             <figure><img src="./img/lanyard-HERO-1-1.jpg" alt="Lanyard Image"></figure>
         </div>
-        <div class="homeTextBox">
-            <figure class="topTextBox"><img src="./img/LC-HomeColorBar-2.png" alt="Color Bar Image"></figure>
-            <article>
+        <div class="homeTextBox flex flex-col items-center bg-blue">
+            <figure class="topTextBox relative bottom-5 w-full"><img class="w-full" src="./img/LC-HomeColorBar-2.png" alt="Color Bar Image"></figure>
+            <article class="text-lg text-white text-center font-medium max-w-3xl pb-4">
                 <p>Lanyard Development, Inc. is the leading fill material supplier in the metropolitan Savannah area. We have broad experience in both the public and private sectors supplying fill material, rock, on-site support and hauling away unsuitable material. We offer competitive pricing with exceptional customer service. We pride ourselves on keeping our commitments to our customers within their deadlines while maintaining strict quality control measures to ensure our customers receive quality products and services.</p>
             </article>
-            <figure class="bottomTextBox"></figure>
         </div>
-        <div class="locationGrid"></div>
+        <figure class="dirtBanner bg-[url('/public/img/dirt.jpg')] h-20"></figure>
+        <div class="locationGrid flex flex-wrap justify-around p-14">
+            <div class="location1 w-80 h-80 bg-[url('/public/img/204.jpg')] bg-cover">
+                <figure class="p-4 relative w-full h-full bg-blue">
+                    <figcaption class="text-white font-bold text-3xl text-center absolute translate-y-[50%] bottom-[50%]">WARNELL BORROW PIT</figcaption>
+                </figure>
+            </div>
+            <div class="location2 w-80 h-80 bg-[url('/public/img/204.jpg')] bg-cover">
+                <figure class="p-4 relative w-full h-full">
+                    <figcaption class="text-white font-bold text-3xl text-center absolute translate-y-[50%] bottom-[50%]">BELFORD AND LEWIS BORROW PITS</figcaption>
+                </figure>
+            </div>
+            <div class="location3 w-80 h-80 bg-[url('/public/img/204.jpg')] bg-cover">
+                <figure class="p-4 relative w-full h-full">
+                    <figcaption class="text-white font-bold text-3xl text-center absolute translate-y-[50%] bottom-[50%]">DAVIS BORROW PIT</figcaption>
+                </figure>
+            </div>
+        </div>
     </div>
 </x-layout>
