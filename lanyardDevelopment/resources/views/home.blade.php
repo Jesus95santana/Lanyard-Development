b841017eb5364b9c2390427c5504d2d52204af3a
jesus95santana <jesus95santana@yahoo.com>
2023-07-19 14:02:05 -0400

    homepage content added

--- lanyardDevelopment/resources/views/home.blade.php
+++ lanyardDevelopment/resources/views/home.blade.php
@@ -1,5 +1,15 @@
 <x-layout>
-    <h1 class="text-3xl font-bold underline">
-        Hello world!
-    </h1>
+    <div class="homeContainer">
+        <div class="imageCarousel">
+            <figure><img src="./img/lanyard-HERO-1-1.jpg" alt="Lanyard Image"></figure>
+        </div>
+        <div class="homeTextBox">
+            <figure class="topTextBox"><img src="./img/LC-HomeColorBar-2.png" alt="Color Bar Image"></figure>
+            <article>
+                <p>Lanyard Development, Inc. is the leading fill material supplier in the metropolitan Savannah area. We have broad experience in both the public and private sectors supplying fill material, rock, on-site support and hauling away unsuitable material. We offer competitive pricing with exceptional customer service. We pride ourselves on keeping our commitments to our customers within their deadlines while maintaining strict quality control measures to ensure our customers receive quality products and services.</p>
+            </article>
+            <figure class="bottomTextBox"></figure>
+        </div>
+        <div class="locationGrid"></div>
+    </div>
 </x-layout>
