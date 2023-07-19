ae4ee03566f5d5d6298f3f336ccb9b1547d43fa4
jesus95santana <jesus95santana@yahoo.com>
2023-07-19 10:26:00 -0400

    added global font | added header responsiveness

--- lanyardDevelopment/resources/views/components/header.blade.php
+++ lanyardDevelopment/resources/views/components/header.blade.php
@@ -1,18 +1,18 @@
 <header class="border-b-[2px] border-black">
-    <div class="headerContainer flex justify-between align-middle m-auto max-w-screen-lg flex-wrap">
+    <div class="headerContainer flex justify-center md:justify-between align-middle m-auto max-w-[900px] flex-wrap">
         <figure class="Logo">
             <img src="./img/LC-LOGO.gif" alt="Logo">
         </figure>
         <nav class="border flex">
             <ul class="flex border items-center">
-                <li class="p-4"><a href="">EXCAVATION SITES</a></li>
-                <li class="p-4"><a href="">CONTACT</a></li>
+                <li class="p-4 font-bold text-gray-500 font-[sans-serif]"><a href="">EXCAVATION SITES</a></li>
+                <li class="p-4 font-bold text-gray-500 font-[sans-serif]"><a href="">CONTACT</a></li>
             </ul>
         </nav>
-        <div class="headerContactContainer flex border items-center">
-            <figure><img src="./img/LD-Phone-ICON.png" alt="Facebook Icon"></figure>
-            <figure><img src="./img/LD-Phone-ICON.png" alt="Phone Icon"></figure>
-            <p>912 330-8351</p>
+        <div class="headerContactContainer flex border items-center p-2">
+            <figure class="p-1"><img src="./img/LD-Phone-ICON.png" alt="Facebook Icon"></figure>
+            <figure class="p-1"><img src="./img/LD-Phone-ICON.png" alt="Phone Icon"></figure>
+            <p class="p-1 text-lg text-gray-500 font-medium">912 330-8351</p>
         </div>
     </div>
 </header>
