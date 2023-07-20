365c5242e2631425a4e8473b65630c1b88da9c8c
jesus95santana <jesus95santana@yahoo.com>
2023-07-20 11:18:55 -0400

    added contact styles

--- lanyardDevelopment/resources/views/contact.blade.php
+++ lanyardDevelopment/resources/views/contact.blade.php
@@ -1,15 +1,15 @@
 <x-layout>
     <div class="contactContainer">
         <figure class="contactHero">
-            <img src="" alt="">
+            <img src="/img/LD-Header-Image-2.jpg" alt="Contact Hero">
         </figure>
-        <div class="contactBanner">
+        <div class="contactBanner w-full bg-blue text-white p-2 text-center font-bold text-4xl">
             <h2>Contact</h2>
         </div>
-        <div class="contactTextBox">
+        <div class="contactTextBox w-full flex flex-col items-center p-8 text-center font-light text-3xl text-gray-500">
             <p>Phone: 912 330-8351</p>
             <p>Fax: 912 330-9160</p>
-            <p>
+            <p class="text-blue font-medium">
                 1000 Towne Center
                 <br>
                 Suite 706
