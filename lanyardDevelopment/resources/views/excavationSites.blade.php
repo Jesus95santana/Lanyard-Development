7e4de887e60e886226f5db35ef9587f38824278b
jesus95santana <jesus95santana@yahoo.com>
2023-07-24 16:14:06 -0400

    Completed excavation page (Missing link to pages)

--- lanyardDevelopment/resources/views/excavationSites.blade.php
+++ lanyardDevelopment/resources/views/excavationSites.blade.php
@@ -18,5 +18,22 @@
             </p>
         </article>
         <figure class="max-w-full "><img class="w-full border" src="/img/LD-NewMap.gif" alt="Lanyard Map"></figure>
+        <div class="excavationBottomGrid flex flex-col items-center p-4">
+            <div class="location1 max-w-4xl pb-10">
+                <figure class="m-auto flex justify-center"><img src="/img/Warnell-2-q9k0qq0cdfnv7wtca3bvda9m244u1kcmz1kh0bzrg8.jpg" alt="Warnell Image"></figure>
+                <h4 class="text-center p-4 font-bold text-gray-400 text-lg">Warnell Borrow Pit</h4>
+                <p class="text-gray-500">The Warnell Borrow Pit is on US Hwy. 80 near Meldrim Road. The entrance is in the Georgia Power Transmission Line Easement at 2059 US Hwy. 80, Bloomingdale, Georgia. Current materials consist of a Sandy Clay top material transitioning to a Granular Sand below. We also have Topsoil and accept clean, uncontaminated earth materials such as muck for disposal. Currently, (7-1-23), we have approximately 800,000 cubic yards available. View Sample Gradation</p>
+            </div>
+            <div class="location2 max-w-4xl pb-10">
+                <figure class="m-auto flex justify-center"><img src="/img/Belford-1-q9k0qn6tsxk092xfqk3znsz89yiqeh1fynm0ki3xyw.jpg" alt="Warnell Image"></figure>
+                <h4 class="text-center p-4 font-bold text-gray-400 text-lg">Belford and Lewis Borrow Pits</h4>
+                <p class="text-gray-500">Both Belford and Lewis Borrow Pits share the same entrance off GA SR 204 just West of Bush Road at 1056 Fort Argyle Road, Savannah, Georgia. Current materials consist of Structural Fill, Granular Sand, Septic Drain Field Sand and Topsoil. Some Clays are still available but in limited quantities. Currently (7-1-23) we have approximatel 2.500,000 cubic vards available in Belford and approximately 800,000 cubic yards in Lewis. We accept clean, uncontaminated earth materials such as muck for disposal. View Sample Gradation</p>
+            </div>
+            <div class="location3 max-w-4xl pb-10">
+                <figure class="m-auto flex justify-center"><img src="/img/Warnell-2-q9k0qq0cdfnv7wtca3bvda9m244u1kcmz1kh0bzrg8.jpg" alt="Warnell Image"></figure>
+                <h4 class="text-center p-4 font-bold text-gray-400 text-lg">Davis Borrow Pit</h4>
+                <p class="text-gray-500">The Davis Borrow Pit is currently in the permitting process and will come online in early 2024. It is located at 2580 Eldora Road in Ellabell, Georgia.</p>
+            </div>
+        </div>
     </div>
 </x-layout>
