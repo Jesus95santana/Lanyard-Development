d27d65e65b118ac313b7c8d8070249858cdd1a86
jesus95santana <jesus95santana@yahoo.com>
2023-07-24 16:02:26 -0400

    halfway complete with excavation site

--- lanyardDevelopment/resources/views/excavationSites.blade.php
+++ lanyardDevelopment/resources/views/excavationSites.blade.php
@@ -6,11 +6,17 @@
         <div class="contactBanner w-full bg-blue text-white p-2 text-center font-bold text-4xl">
             <h2>Services</h2>
         </div>
-        <article class="p-10">
-            <h2>WE ARE A LEADING SUPPLIER OF DIRT AND SOIL TO SAVANNAH AND SURROUNDING AREAS</h2>
-            <p>We sell screened topsoil by the dump truck load (14 cubic yards loose/10.5 cubic yards compacted).
-                Pick up available for dump trucks or dump trailers. We offer delivery to the Savannah, Pooler,
-                Effingham, Richmond hill, and surrounding areas.</p>
+        <article class="p-12 max-w-5xl m-auto">
+            <h2 class="font-bold text-gray-500 text-2xl">WE ARE A LEADING SUPPLIER OF DIRT AND SOIL TO SAVANNAH AND SURROUNDING AREAS</h2>
+            <p class="text-gray-500 text-xl pt-5">
+                We sell screened topsoil by the dump truck load (14 cubic yards loose/10.5 cubic yards compacted).
+                Pick up available for dump trucks or dump trailers.
+                <span class="font-bold text-gray-500 text-xl">
+                    We offer delivery to the Savannah, Pooler,
+                    Effingham, Richmond hill, and surrounding areas.
+                </span>
+            </p>
         </article>
+        <figure class="max-w-full "><img class="w-full border" src="/img/LD-NewMap.gif" alt="Lanyard Map"></figure>
     </div>
 </x-layout>
